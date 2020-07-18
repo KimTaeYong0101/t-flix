a9b41a31772ca3c4bcc093aae33c20e29689d9e6
gray <xodyd1756@naver.com>
2020-07-18 16:19:48 +0900

    Updates

---
+++ precache-manifest.acbdaa9d9ee1d40e075ec362bec1ed77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a627fa19a19167d4b166e1b59d74d5d",
+    "url": "/tflix/index.html"
+  },
+  {
+    "revision": "6c96f41c3126b6c79007",
+    "url": "/tflix/static/js/2.b93632f8.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/tflix/static/js/2.b93632f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dd27d686dc816cadc0",
+    "url": "/tflix/static/js/main.75d945f7.chunk.js"
+  },
+  {
+    "revision": "c53c5da29bd182095410",
+    "url": "/tflix/static/js/runtime-main.cc8450ab.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/tflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
